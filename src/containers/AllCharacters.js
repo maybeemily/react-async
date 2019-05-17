19fc65a9c8fd9c7cf8c35ca1e04ee4859c37fd78
emily baier <emilyrbaier@gmail.com>
2019-05-17 09:51:26 -0700

    components all made, not sure if it works yet because API is still being blocked

--- src/containers/AllCharacters.js
+++ src/containers/AllCharacters.js
@@ -2,28 +2,53 @@ import React, { PureComponent } from 'react';
 import Characters from '../components/Characters';
 import Loading from '../components/Loading';
 import { getCharacters } from '../services/RickMortyApi';
+import Paging from '../components/Paging/Paging';
 
 export default class AllCharacters extends PureComponent {
   state = {
     characters: [],
-    loading: true
+    loading: true,
+    totalPages: 1,
+    currentPage: 1
   }
 
   fetchCharacters = () => {
     this.setState({ loading: true });
-    getCharacters()
-      .then(({ characters }) => this.setState({ characters, loading: false }));
+    getCharacters(this.state.currentPage)
+      .then(({ totalPages, characters }) => this.setState({ 
+        characters, 
+        loading: false,
+        totalPages
+      }));
+  }
+
+  nextPage = page => {
+    this.setState({ currentPage: page });
   }
 
   componentDidMount() {
     this.fetchCharacters();
   }
 
-  render() {
-    const { characters, loading } = this.state;
-    if(loading) return <Loading />;
-
-    return <Characters characters={characters}/>;
+  componentDidUpdate(prevProps, prevState) {
+    if(prevState.currentPage !== this.state.currentPage) {
+      this.fetchCharacters();
+    }
   }
 
+  render() {
+    const { 
+      characters, 
+      loading,
+      totalPages,
+      currentPage
+    } = this.state;
+    if(loading) return <Loading />;
+
+    return (
+      <Paging currentPage = {currentPage} totalPages={totalPages} nextPage={this.nextPage}>
+        <Characters characters={characters}/>;
+      </Paging>
+    );
+  }
 }
